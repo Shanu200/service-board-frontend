e9337bf8f1b5a17200f875942ad98e561bf99963
Shanuka Wanniarachchi <shanukawanniarachchi2000@gmail.com>
2026-05-21 22:30:27 +0530

    versel_sensitive_ErrorFixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
-import Navbar from "@/components/layout/navBar";
+import Navbar from "@/components/layout/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
